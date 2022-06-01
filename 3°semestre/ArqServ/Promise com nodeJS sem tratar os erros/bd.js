ca0b62cf8df49bb74995ef4a939cb4e3e7dce843
Jonas S. Laviani <89589588+lavianii@users.noreply.github.com>
2022-06-01 13:47:23 -0300

    a

--- 3°semestre/ArqServ/Promise com nodeJS sem tratar os erros/bd.js
+++ 3°semestre/ArqServ/Promise com nodeJS sem tratar os erros/bd.js
@@ -2,15 +2,16 @@ async function getConexao(){
     if(global.conexao && global.conexao.state !== 'disconnected')
         return global.conexao;
     
-
+    //importa os pacotes do node mysql2 e promise
     const mysql      = require('mysql2/promise');
+    //importa as váriaveis de ambiente
     const bdConfig   = require('./bdconfig.js');
-
+    //cria a conexão
     const conexao = await mysql.createConnection(bdConfig);
     global.conexao = conexao;
         return conexao;
 }
-
+//métod que contém o comando para criar a tabela
 async function estrurureSe(){
     const conexao = await getConexao();
 
