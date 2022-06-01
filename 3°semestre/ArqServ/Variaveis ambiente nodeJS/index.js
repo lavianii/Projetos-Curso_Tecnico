5f9d8132e7c6ed99d967e4b9913e8cd34e212774
Jonas S. Laviani <89589588+lavianii@users.noreply.github.com>
2022-06-01 11:41:58 -0300

    adicionando comentários

--- 3°semestre/ArqServ/Variaveis ambiente nodeJS/index.js
+++ 3°semestre/ArqServ/Variaveis ambiente nodeJS/index.js
@@ -1,15 +1,22 @@
+// pacote que usei do node para o banco mysql
 let mysql = require('mysql2');
+
+//para utilizar a variavel de ambiente configurada
 let bdConfig = require('./bdconfig.js');
 
+//cria a conexão
 let conexao = mysql.createConnection(bdConfig);
+// faz a conexão
 conexao.connect();
 
+//faz o comando sql
 conexao.query('select * from cores', function (erro, resultado, tabela) {
   if (erro)
   {
     console.log('Problema na conexão');
   } else 
   {
+    //para o ponteiro percorrer pela tabela e mostrar no cmd
     for (let i = 0; i < resultado.length; i++) {
       console.log(resultado[i].id, resultado[i].nome);
     }
@@ -17,13 +24,15 @@ conexao.query('select * from cores', function (erro, resultado, tabela) {
 });
 
 conexao.end();
+
+//segunda maneira de fazer mas sem as variaveis de ambiente
 /*
 let mysql = require("mysql2");
 let connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Jo01082002!@#",
-  database: "teste",
+  host: "seu host",
+  user: "seu user",
+  password: "sua senha",
+  database: "seu banco",
 });
 
 connection.query("select * from cores", function (erro, resultado, tabela) {
