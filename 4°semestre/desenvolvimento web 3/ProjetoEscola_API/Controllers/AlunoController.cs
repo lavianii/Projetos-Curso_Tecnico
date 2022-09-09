28e30ab65b75caa6a0439bc3e606676399f25a7d
JONAS SOARES LAVIANI <u21773@LAPA33.academico.cotuca.unicamp.br>
2022-09-09 20:49:52 -0300

    dotnet

---
+++ 4°semestre/desenvolvimento web 3/ProjetoEscola_API/Controllers/AlunoController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using ProjetoEscola_API.Data;
+using ProjetoEscola_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoEscola_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+
+        private EscolaContext _context;
+        public AlunoController(EscolaContext context)
+        {
+            //construtor
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<List<Aluno>> GetAll(){
+            return _context.Aluno.ToList();
+        }
+    }
+}
---
+++ 4°semestre/desenvolvimento web 3/ProjetoEscola_API/Data/EscolaContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoEscola_API.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ProjetoEscola_API.Data
+{
+    public class EscolaContext : DbContext{
+        protected readonly IConfiguration Configuration;
+
+        public EscolaContext(IConfiguration configuration){
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options){
+            options.UseSqlServer(Configuration.GetConnectionString("StringConexaoSQLServer"));
+        }
+
+        public DbSet<Aluno>? Aluno { get; set; }
+    }
+
+}
