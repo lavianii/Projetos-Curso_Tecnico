2c9cb950c8f89bdec69c0b41b53b699756e563b8
JONAS SOARES LAVIANI <u21773@dinalva49.academico.cotuca.unicamp.br>
2022-09-12 22:06:04 -0300

    dotnet

--- 4°semestre/desenvolvimento web 3/ProjetoEscola_API/Controllers/AlunoController.cs
+++ 4°semestre/desenvolvimento web 3/ProjetoEscola_API/Controllers/AlunoController.cs
@@ -1,24 +1,90 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoEscola_API.Data;
 using ProjetoEscola_API.Models;
-using Microsoft.EntityFrameworkCore;
+
 
 namespace ProjetoEscola_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AlunoController : ControllerBase
+    public class AlunoController : Controller
     {
 
-        private EscolaContext _context;
+        private readonly EscolaContext _context;
         public AlunoController(EscolaContext context)
         {
             //construtor
             _context = context;
         }
+
         [HttpGet]
-        public ActionResult<List<Aluno>> GetAll(){
+        public ActionResult<List<Aluno>> GetAll()
+        {
             return _context.Aluno.ToList();
+        }
+
+        [HttpGet("AlunoId")]
+        public ActionResult<List<Aluno>> Get(int AlunoId)
+        {
+            try
+            {
+                var result = _context.Aluno.Find(AlunoId);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha no acesso ao banco de dados.");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> post(Aluno model)
+        {
+            try
+            {
+                _context.Aluno.Add(model);
+                if (await _context.SaveChangesAsync() == 1)
+                {
+                    //return Ok();
+                    return Created($"/api/aluno/{model.ra}", model);
+                }
+            }
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha no acesso ao banco de dados.");
+            }
+            // retorna BadRequest se não conseguiu incluir
+            return BadRequest();
+        }
+
+        [HttpDelete("{AlunoId}")]
+        public async Task<ActionResult> delete(int AlunoId)
+        {
+            try
+            {
+                //verifica se existe aluno a ser excluído
+                var aluno = await _context.Aluno.FindAsync(AlunoId);
+                if (aluno == null)
+                {
+                    //método do EF
+                    return NotFound();
+                }
+                _context.Remove(aluno);
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha no acesso ao banco de dados.");
+            }
         }
     }
 }
