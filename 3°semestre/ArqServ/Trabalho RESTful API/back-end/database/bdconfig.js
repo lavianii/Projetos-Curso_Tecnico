8ce524450c5be0f557a8b3efd0d9a1412676340c
lavianii <jonaslavi02@gmail.com>
2022-09-22 16:09:05 -0300

    a

--- 3°semestre/ArqServ/Trabalho RESTful API/back-end/database/bdconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    host: process.env.NODE_MYSQL_SERVIDOR,
-    user: process.env.NODE_MYSQL_USUARIO,
-    password: process.env.NODE_MYSQL_SENHA,
-    database: process.env.NODE_MYSQL_BD
-  };
