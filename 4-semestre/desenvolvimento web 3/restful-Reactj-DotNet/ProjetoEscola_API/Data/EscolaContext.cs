ecba7821a53b2140d91e0747b2cefbe38bbe6db9
lavianii <jonaslavi02@gmail.com>
2022-10-21 20:25:05 -0300

    a

--- 4-semestre/desenvolvimento web 3/restful-Reactj-DotNet/ProjetoEscola_API/Data/EscolaContext.cs
+++ 4-semestre/desenvolvimento web 3/restful-Reactj-DotNet/ProjetoEscola_API/Data/EscolaContext.cs
@@ -1,21 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using ProjetoEscola_API.Models;
-using System.Diagnostics.CodeAnalysis;
-
 namespace ProjetoEscola_API.Data
 {
-    public class EscolaContext : DbContext{
+    public class EscolaContext : DbContext
+    {
         protected readonly IConfiguration Configuration;
-
-        public EscolaContext(IConfiguration configuration){
+        public EscolaContext(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options){
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server with connection string from app settings
             options.UseSqlServer(Configuration.GetConnectionString("StringConexaoSQLServer"));
         }
-
         public DbSet<Aluno>? Aluno { get; set; }
+        public DbSet<User>? Usuario { get; set; }
     }
-
 }
