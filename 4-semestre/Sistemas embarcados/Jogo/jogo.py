423ba703956884dd07c9df35ce7071adce363f5c
lavianii <jonaslavi02@gmail.com>
2022-11-29 14:10:30 -0300

    Controlando as bordas

--- 4-semestre/Sistemas embarcados/Jogo/jogo.py
+++ 4-semestre/Sistemas embarcados/Jogo/jogo.py
@@ -5,21 +5,20 @@ from sys import exit
 from random import randint
 
 pygame.init()
-
-perdeu_font = pygame.font.SysFont('arial', 100, True, True)
 def game_over():
-    # Renderiza o Texto
-    game_over_text = perdeu_font.render(f"GAME OVER", True, (200, 200, 200))
+  # Renderiza o Texto
+  perdeu_font = pygame.font.SysFont('arial', 100, True, True)
+  game_over_text = perdeu_font.render(f"GAME OVER", True, (200, 200, 200))
 
-    # Desenhando os elementos
-    tela.fill((0, 0, 0))
-    tela.blit(game_over_text, (largura/2 - (game_over_text.get_width()/2),
-              altura/2 - game_over_text.get_height()/2))
+  # Desenhando os elementos
+  tela.fill((0, 0, 0))
+  tela.blit(game_over_text, (largura/2 - (game_over_text.get_width()/2),
+            altura/2 - game_over_text.get_height()/2))
 
-    pygame.display.update()
-    sleep(3)
-    pygame.quit()
-    exit()
+  pygame.display.update()
+  sleep(3)
+  pygame.quit()
+  exit()
 
 
 # Construindo a tela
@@ -61,11 +60,12 @@ fps = pygame.time.Clock()
 
 janela_aberta = True
 while janela_aberta:
-
+  esta_rodando = True
   for event in pygame.event.get():
     if event.type == QUIT:
-      pygame.quit()
-      exit()
+      janela_aberta = False
+      esta_rodando = False
+
 
   # Velocidade do jogo
   fps.tick(60)
@@ -89,34 +89,41 @@ while janela_aberta:
   if pygame.key.get_pressed()[K_DOWN]:
     movimento_y += 5
 
-  #cria o personagem
+  # Cria o personagem
   personagem = pygame.draw.rect(tela, (0, 255, 0), (movimento_x, movimento_y, 20, 20))
 
-  #cria a comida
+  # Cria a comida
   comida = pygame.draw.rect(tela, (0, 0, 255), (x_comida, y_comida, 20, 20))
 
-  #cria os inimigos
+  # Cria os inimigos
   inimigo1 = pygame.draw.rect(tela, (255, 0, 0), (x_inimigo1, y_inimigo1, 50, 50))
   inimigo2 = pygame.draw.rect(tela, (255, 255, 0), (x_inimigo2, y_inimigo2, 50, 50))
   inimigo3 = pygame.draw.circle(tela, (0, 255, 255), (x_inimigo3, y_inimigo3),50)
   inimigo4 = pygame.draw.circle(tela, (0, 255, 255), (x_inimigo4, y_inimigo4),50)
 
-  #circulo vertical
+  # Circulo vertical
   if y_inimigo3 >= altura:
     y_inimigo3 = 0
   else:
     y_inimigo3 += 3
   
-  #circulo horizontal
+  # Circulo horizontal
   if x_inimigo4 >= altura:
     x_inimigo4 = 0
   else:
     x_inimigo4 += 3
   
+  # Controla as bordas  
   if movimento_x >= 600:
     game_over()
+  if movimento_x <= 0:
+    game_over()
+  if movimento_y >= 600:
+    game_over()
+  if movimento_y <= 0:
+    game_over()
 
-  # detecta a colisão do inimigo 1
+  # Detecta a colisão do inimigo 1
   if inimigo1.colliderect(personagem):
     x_inimigo1 = randint(40, 600)
     y_inimigo1 = randint(50, 600)
@@ -125,7 +132,7 @@ while janela_aberta:
       tela.fill((0, 0, 0))
       game_over()
 
-  # detecta a colisão do inimigo 2
+  # Detecta a colisão do inimigo 2
   if inimigo2.colliderect(personagem):
     x_inimigo2 = randint(40, 600)
     y_inimigo2 = randint(50, 600)
@@ -134,14 +141,14 @@ while janela_aberta:
       tela.fill((0, 0, 0))
       game_over()
 
-  # detecta a colisão do inimigo 3
+  # Detecta a colisão do inimigo 3
   if inimigo3.colliderect(personagem):
     pontos -= 2
     if pontos < 0:
       tela.fill((0, 0, 0))
       game_over()
 
-  # detecta colisão da comida
+  # Detecta colisão da comida
   if comida.colliderect(personagem):
     x_comida = randint(40, 600)
     y_comida = randint(50, 600)
